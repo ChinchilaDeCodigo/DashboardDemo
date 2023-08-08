5884f07fd9e866512b7ce4124fc4d81615b3afa2
Leonardo Victor <leonardo.victorms@gmail.com>
2023-08-08 12:15:16 -0300

    Primeiro commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './componentes/dashboard/dashboard.component';
+import { ComponenteAComponent } from './componentes/componente-a/componente-a.component';
+import { ComponenteBComponent } from './componentes/componente-b/componente-b.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: DashboardComponent
+  },
+  {
+    path: 'componentA',
+    component: ComponenteAComponent
+  },
+  {
+    path: 'componentB',
+    component: ComponenteBComponent
+  },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
